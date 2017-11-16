e05fb21ebbf54dfa8a658af1387628cc76e3a257
Mathias Schreck <schreck.mathias@googlemail.com>
2017-11-16 18:41:51 +0100

    Infer ramda method names in filter-simplification

--- rules/filter-simplification.js
+++ rules/filter-simplification.js
@@ -1,13 +1,16 @@
 'use strict';
 
+const R = require('ramda');
+const utils = require('../lib/utils');
+
+
 const create = context => ({
     CallExpression(node) {
-        if (node.callee.type === 'Identifier'
-            && node.callee.name === 'filter'
-            && node.arguments.length > 0
-            && node.arguments[0].type === 'CallExpression'
-            && node.arguments[0].callee.type === 'Identifier'
-            && node.arguments[0].callee.name === 'complement') {
+        const currentScope = context.getScope();
+        const isRamdaFilterCall = R.propSatisfies(utils.isRamdaMethod('filter', currentScope), 'callee');
+        const isRamdaFilterCallExpression = R.allPass([ utils.isCallExpression, isRamdaFilterCall ]);
+
+        if (utils.isRamdaMethod('complement', currentScope, node.callee) && isRamdaFilterCallExpression(node.parent)) {
             context.report({
                 node,
                 message: '`filter(complement(_))` should be simplified to `reject(_)`'
--- test/filter-simplification.js
+++ test/filter-simplification.js
@@ -13,14 +13,188 @@ const ruleTester = avaRuleTester(test, {
 
 ruleTester.run('filter-simplification', rule, {
     valid: [
-        'filter(condition)'
+        'filter(condition)',
+        'R.filter(predicate)',
+        'R.filter(predicate, [])',
+        'R.filter(predicate, data)',
+        'const Ramda = require("ramda"), R = { filter: "foo" }; R.filter(Ramda.complement(predicate));',
+        'const R = { filter: "foo", complement: "bar" }; R.filter(R.complement(predicate));',
+        'const R = require("ramda"); function foo() { const R = require("not-ramda"); R.filter(R.complement(predicate)); }',
+        'let window; window.R.filter(window.R.complement(predicate));',
+        'let global; global.R.filter(global.R.complement(predicate));',
+        'foo.R.filter(foo.R.complement(predicate));',
+        'window.R().filter(window.R().complement(predicate));',
+        'const R = require("ramda", "invalidSecondArg"); R.filter(R.complement(predicate));',
+        'const R = require(basePath + "/ramda"); R.filter(R.complement(predicate));',
+        'import { filter as something, complement } from "ramda"; something(complement(predicate));',
+        'import { filter, complement } from "not-ramda"; filter(complement(predicate));',
+        'import R from "not-ramda"; R.filter(R.complement(predicate));',
+        'function foo(filter, complement) { filter(complement(predicate)); }',
+        'const R = notRequire("ramda"); R.filter(R.complement(predicate));'
     ],
     invalid: [
         {
-            code: 'filter(complement(even))',
+            code: 'const { filter, complement } = require("ramda"); filter(complement(even))',
             errors: [{
                 ruleId: 'filter-simplification',
                 message: '`filter(complement(_))` should be simplified to `reject(_)`'
+            }]
+        },
+        {
+            code: 'const { filter, complement } = require("ramda"); filter(complement(even), data)',
+            errors: [{
+                ruleId: 'filter-simplification',
+                message: '`filter(complement(_))` should be simplified to `reject(_)`'
+            }]
+        },
+        {
+            code: 'const R = require("ramda"); R.filter(R.complement(predicate))',
+            errors: [{
+                ruleId: 'filter-simplification',
+                message: '`filter(complement(_))` should be simplified to `reject(_)`',
+                line: 1,
+                column: 38
+            }]
+        },
+        {
+            code: 'const R = require("ramda"); R["filter"](R["complement"](predicate))',
+            errors: [{
+                ruleId: 'filter-simplification',
+                message: '`filter(complement(_))` should be simplified to `reject(_)`',
+                line: 1,
+                column: 41
+            }]
+        },
+        {
+            code: [
+                'const R = require("ramda");',
+                'function foo() {',
+                '    const R = require("not-ramda");',
+                '    R.filter(R.complement(predicate));',
+                '}',
+                'R.filter(R.complement(predicate));'
+            ].join('\n'),
+            errors: [{
+                ruleId: 'filter-simplification',
+                message: '`filter(complement(_))` should be simplified to `reject(_)`',
+                line: 6,
+                column: 10
+            }]
+        },
+        {
+            code: [
+                'const R = require("ramda");',
+                'function foo() {',
+                '    R.filter(R.complement(predicate));',
+                '}',
+                'R.filter(R.complement(predicate));'
+            ].join('\n'),
+            errors: [
+                {
+                    ruleId: 'filter-simplification',
+                    message: '`filter(complement(_))` should be simplified to `reject(_)`',
+                    line: 3,
+                    column: 14
+                },
+                {
+                    ruleId: 'filter-simplification',
+                    message: '`filter(complement(_))` should be simplified to `reject(_)`',
+                    line: 5,
+                    column: 10
+                }
+            ]
+        },
+        {
+            code: 'R.filter(R.complement(predicate));',
+            errors: [{
+                ruleId: 'filter-simplification',
+                message: '`filter(complement(_))` should be simplified to `reject(_)`',
+                line: 1,
+                column: 10
+            }]
+        },
+        {
+            code: 'window.R.filter(window.R.complement(predicate));',
+            errors: [{
+                ruleId: 'filter-simplification',
+                message: '`filter(complement(_))` should be simplified to `reject(_)`',
+                line: 1,
+                column: 17
+            }]
+        },
+        {
+            code: 'global.R.filter(global.R.complement(predicate));',
+            errors: [{
+                ruleId: 'filter-simplification',
+                message: '`filter(complement(_))` should be simplified to `reject(_)`',
+                line: 1,
+                column: 17
+            }]
+        },
+        {
+            code: [
+                'const R = require("ramda");',
+                'const foo = R;',
+                'const bar = foo;',
+                'bar.filter(bar.complement(predicate));'
+            ].join('\n'),
+            errors: [{
+                ruleId: 'filter-simplification',
+                message: '`filter(complement(_))` should be simplified to `reject(_)`',
+                line: 4,
+                column: 12
+            }]
+        },
+        {
+            code: [
+                'import R from "ramda";',
+                'R.filter(R.complement(predicate));'
+            ].join('\n'),
+            errors: [{
+                ruleId: 'filter-simplification',
+                message: '`filter(complement(_))` should be simplified to `reject(_)`',
+                line: 2,
+                column: 10
+            }]
+        },
+        {
+            code: [
+                'import Ramda from "ramda";',
+                'Ramda.filter(Ramda.complement(predicate));'
+            ].join('\n'),
+            errors: [{
+                ruleId: 'filter-simplification',
+                message: '`filter(complement(_))` should be simplified to `reject(_)`',
+                line: 2,
+                column: 14
+            }]
+        },
+        {
+            code: [
+                'import { filter, complement } from "ramda";',
+                'filter(complement(predicate));'
+            ].join('\n'),
+            errors: [{
+                ruleId: 'filter-simplification',
+                message: '`filter(complement(_))` should be simplified to `reject(_)`',
+                line: 2,
+                column: 8
+            }]
+        },
+        {
+            code: [
+                'import { filter, complement } from "ramda";',
+                'function foo() {',
+                '    let filter, complement;',
+                '    filter(complement(predicate));',
+                '}',
+                'filter(complement(predicate));'
+            ].join('\n'),
+            errors: [{
+                ruleId: 'filter-simplification',
+                message: '`filter(complement(_))` should be simplified to `reject(_)`',
+                line: 6,
+                column: 8
             }]
         }
     ]
