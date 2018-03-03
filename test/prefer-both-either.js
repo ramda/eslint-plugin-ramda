7e18afde732d389ad78ffd7a49f9f8eed72ab7da
Jonathan Goldwasser <jonathan.goldwasser@gmail.com>
2018-03-03 16:08:16 +0100

    Add more tests for prefer-both-either

--- test/prefer-both-either.js
+++ test/prefer-both-either.js
@@ -16,9 +16,9 @@ const prefer = {
     anyPass: 'either'
 }
 
-const error = (instead, args) => ({
+const error = instead => ({
     ruleId: 'prefer-both-either',
-    message: `Instead of \`${instead}([${args}])\`, prefer \`${prefer[instead]}(${args})\``
+    message: `Instead of \`${instead}\`, prefer \`${prefer[instead]}\` when there are only two predicates`
 });
 
 ruleTester.run('prefer-both-either', rule, {
@@ -35,19 +35,31 @@ ruleTester.run('prefer-both-either', rule, {
     invalid: [
         {
             code: 'allPass([foo, bar])',
-            errors: [error('allPass', 'foo, bar')]
+            errors: [error('allPass')]
         },
         {
             code: 'allPass([foo, bar], baz)',
-            errors: [error('allPass', 'foo, bar')]
+            errors: [error('allPass')]
+        },
+        {
+            code: 'allPass([(foo) => !foo, function () { return false; }])',
+            errors: [error('allPass')]
+        },
+        {
+            code: 'allPass([complement(foo), complement(bar)])',
+            errors: [error('allPass')]
         },
         {
             code: 'anyPass([foo, bar])',
-            errors: [error('anyPass', 'foo, bar')]
+            errors: [error('anyPass')]
         },
         {
             code: 'anyPass([foo, bar], baz)',
-            errors: [error('anyPass', 'foo, bar')]
+            errors: [error('anyPass')]
+        },
+        {
+            code: 'anyPass([R.T, R.F])',
+            errors: [error('anyPass')]
         }
     ]
 });
