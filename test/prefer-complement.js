c6e9bd3f27af99a719227bf2039850d82b66d681
Jonathan Goldwasser <jonathan@goldwasserexchange.be>
2018-03-05 10:47:16 +0100

    Simplify and fix error reporting for prefer-complement

--- test/prefer-complement.js
+++ test/prefer-complement.js
@@ -11,11 +11,14 @@ const ruleTester = avaRuleTester(test, {
     }
 });
 
-const instead = (composition, fn) => (composition === 'compose' ? `compose(not, ${fn})` : `pipe(${fn}, not)`);
+const instead = {
+    compose: 'compose(not, ...)',
+    pipe: 'pipe(..., not)'
+};
 
-const error = (composition, fn) => ({
-  ruleId: 'prefer-complement',
-  message: `Instead of \`${instead(composition, fn)}\`, prefer \`complement(${fn})\``
+const error = composition => ({
+    ruleId: 'prefer-complement',
+    message: `Instead of \`${instead[composition]}\`, prefer \`complement(...)\``
 });
 
 ruleTester.run('prefer-complement', rule, {
@@ -33,35 +36,59 @@ ruleTester.run('prefer-complement', rule, {
     invalid: [
         {
             code: 'compose(not, isEmpty)',
-            errors: [error('compose', 'isEmpty')]
+            errors: [error('compose')]
+        },
+        {
+            code: 'compose(not, xs => xs.length)',
+            errors: [error('compose')]
         },
         {
             code: 'pipe(isEmpty, not)',
-            errors: [error('pipe', 'isEmpty')]
+            errors: [error('pipe')]
+        },
+        {
+            code: 'pipe(function (xs) { return xs.length === 0; }, not)',
+            errors: [error('pipe')]
         },
         {
             code: 'compose(not, isNil)',
-            errors: [error('compose', 'isNil')]
+            errors: [error('compose')]
+        },
+        {
+            code: 'compose(not, R.isNil)',
+            errors: [error('compose')]
         },
         {
             code: 'pipe(isNil, not)',
-            errors: [error('pipe', 'isNil')]
+            errors: [error('pipe')]
+        },
+        {
+            code: 'pipe(R.isNil, not)',
+            errors: [error('pipe')]
         },
         {
             code: 'compose(not, foo)',
-            errors: [error('compose', 'foo')]
+            errors: [error('compose')]
+        },
+        {
+            code: 'compose(not, foo(bar))',
+            errors: [error('compose')]
         },
         {
             code: 'pipe(foo, not)',
-            errors: [error('pipe', 'foo')]
+            errors: [error('pipe')]
+        },
+        {
+            code: 'pipe(foo(bar), not)',
+            errors: [error('pipe')]
         },
         {
             code: 'propSatisfies(compose(not, isNil))',
-            errors: [error('compose', 'isNil')]
+            errors: [error('compose')]
         },
         {
             code: 'propSatisfies(pipe(isNil, not))',
-            errors: [error('pipe', 'isNil')]
+            errors: [error('pipe')]
         }
     ]
 });
